80d477cac0f2dbb02332701733f7af149308a9ef
Michael C. Harris <michael@twofishcreative.com>
2011-07-10 10:17:47 +0000

    detab.

--- home.php
+++ home.php
@@ -1,9 +1,9 @@
 <?php $theme->display('header'); ?>
 
-					<?php
-					foreach ( $posts as $post ) {
-						$theme->content($post);
-					}
-					?>
+	<?php
+	foreach ( $posts as $post ) {
+		$theme->content($post);
+	}
+	?>
 
 <?php $theme->display('footer'); ?>
